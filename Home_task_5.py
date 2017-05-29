8db614c25a11c0fa4fe0df1b018a8ecf0a3d0b97
Elvis Mongo <elvisdep@mail.ru>
2017-05-29 19:28:55 +0300

    first commit

---
+++ Home_task_5.py
@@ -0,0 +1,30 @@
+def Function(x, y, q):
+    q = ''
+    print('Enter coordinate x')
+    x = int(input(''))
+    print('Enter coordinate y')
+    y = int(input(''))
+    if (x>0) and (y>0):
+        q = 'Point is located in the first quarter'
+        return q
+    elif (x<0) and (y>0):
+        q = 'Point is located in the second quarter'
+        return q
+    elif (x<0) and (y<0):
+        q = 'Point is located in the third quarter'
+        return q
+    elif (x>0) and (y<0):
+        q = 'Point is located in the fourth quarter'
+        return q
+    elif (x==0) and (y==0):
+        q = 'Point is located in the origin of the coordinates'
+        return q
+    else:
+        q = 'Point is located on the one of the axes'
+        return q
+
+x = int()
+y = int()
+q = str()
+
+print(Function(x, y, q))
