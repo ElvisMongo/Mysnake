c1dab25036b0ba786129af6a79026290a0fd05c6
Elvis Mongo <elvisdep@mail.ru>
2017-06-02 11:42:46 +0300

    Исполняемый файл для модуля systems_of_counts.py

---
+++ main1.py
@@ -0,0 +1,8 @@
+import system_of_counts
+from system_of_counts import delenie
+
+x = int(input('Vvedite chislo: ' ))
+n = int(input('Vvedite osnovanie system ischislenia: '))
+
+delenie(x, n)
+print(delenie(x, n))
